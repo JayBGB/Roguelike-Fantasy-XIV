baed1078cb4c8807646ada57f93a9a6b1ea2727e
Ruben Nuñez Gonzalez <rnunezgonzalez@danielcastelao.org>
2023-03-07 08:50:11 +0100

    More UI changes

--- PruebaProyecto/src/com/project/ui/UI.java
+++ PruebaProyecto/src/com/project/ui/UI.java
@@ -11,17 +11,22 @@ public class UI extends JFrame {
             setSize(1280,720);
             setLocationRelativeTo(null);
             setVisible(true);
+            JPanel attackBar = new JPanel();
+            JLabel random = new JLabel("TextoTextoTextoTextoTextoTextoTextoTexto");
             JButton atck = new JButton("Attack");
             JButton def = new JButton("Def");
             JButton ab = new JButton("Abilities");
             JButton it = new JButton("Items");
-            atck.setPreferredSize(new Dimension(200,100));
-            def.setPreferredSize(new Dimension(200,100));
-            ab.setPreferredSize(new Dimension(200,100));
-            it.setPreferredSize(new Dimension(200,100));
-            add(atck, BorderLayout.SOUTH);
-            atck.add(it);
-            it.add(def);
-            def.add(ab);
+            atck.setPreferredSize(new Dimension(100,50));
+            def.setPreferredSize(new Dimension(100,50));
+            ab.setPreferredSize(new Dimension(100,50));
+            it.setPreferredSize(new Dimension(100,50));
+            atck.setLocation(0,0);
+            attackBar.add(atck);
+            attackBar.add(it);
+            attackBar.add(def);
+            attackBar.add(ab);
+            add(attackBar, BorderLayout.SOUTH);
+            attackBar.add(random);
       }
 }
