cdc1c8b6d201d873f934d13e81f2a352c0c2e0c6
Ruben Nuñez Gonzalez <rnunezgonzalez@danielcastelao.org>
2023-03-07 12:12:11 +0100

    More UI updates

--- PruebaProyecto/src/com/project/ui/UI.java
+++ PruebaProyecto/src/com/project/ui/UI.java
@@ -1,5 +1,7 @@
 package com.project.ui;
 
+import com.project.game.Main;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,25 +10,63 @@ public class UI extends JFrame {
       public UI(){
             super("MainScreen");
             setLayout(new BorderLayout());
-            setSize(1280,720);
-            setLocationRelativeTo(null);
-            setVisible(true);
+            setPreferredSize(new Dimension(1280,720));
+            this.setLocationRelativeTo(null);
+            this.setVisible(true);
+            this.setLocation(GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint());
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            //PJ panel
+            JPanel pj = new JPanel();
+            pj.setLayout(new BoxLayout(pj,BoxLayout.PAGE_AXIS));
+            pj.setBorder(BorderFactory.createEmptyBorder(50,0,50,50));
+            JLabel line = new JLabel("-----------------");
+            JLabel hp = new JLabel("HP: " + Main.data.getMaxHp() + "/" + Main.data.getHp());
+            hp.setBorder(BorderFactory.createEmptyBorder(10,0,10,10));
+            JLabel mana = new JLabel("Mana: " + Main.data.getMaxMana() + "/" + Main.data.getMana());
+            mana.setBorder(BorderFactory.createEmptyBorder(0,0,10,10));
+            JLabel defense = new JLabel("Defense: " + Main.data.getDefense());
+            defense.setBorder(BorderFactory.createEmptyBorder(0,0,10,10));
+            JLabel strLabel = new JLabel("Strngth: " + Main.data.getAttStrength());
+            strLabel.setBorder(BorderFactory.createEmptyBorder(0,0,10,10));
+            strLabel.setFont(new Font("Arial", Font.PLAIN, 12));
+            JLabel dexLabel = new JLabel("Dexterity: " + Main.data.getAttDexterity());
+            dexLabel.setBorder(BorderFactory.createEmptyBorder(0,0,10,10));
+            dexLabel.setFont(new Font("Serif", Font.PLAIN, 12));
+            JLabel conLabel = new JLabel("Constitution: " + Main.data.getAttConstitution());
+            conLabel.setBorder(BorderFactory.createEmptyBorder(0,0,10,10));
+            conLabel.setFont(new Font("Serif", Font.PLAIN, 12));
+            JLabel wisLabel = new JLabel("Wisdom: " + Main.data.getAttWisdom());
+            wisLabel.setBorder(BorderFactory.createEmptyBorder(0,0,10,10));
+            wisLabel.setFont(new Font("Serif", Font.PLAIN, 12));
+            pj.add(hp);
+            pj.add(mana);
+            pj.add(defense);
+            pj.add(line);
+            pj.add(strLabel);
+            pj.add(dexLabel);
+            add(pj, BorderLayout.EAST);
+
+
             JPanel attackBar = new JPanel();
-            JLabel random = new JLabel("TextoTextoTextoTextoTextoTextoTextoTexto");
+            attackBar.setOpaque(false);
+            JProgressBar hpBar = new JProgressBar();
+            JComboBox abBar = new JComboBox<String>(new String[]{"1","2"});
             JButton atck = new JButton("Attack");
             JButton def = new JButton("Def");
             JButton ab = new JButton("Abilities");
-            JButton it = new JButton("Items");
+            JButton run = new JButton("Run");
             atck.setPreferredSize(new Dimension(100,50));
             def.setPreferredSize(new Dimension(100,50));
             ab.setPreferredSize(new Dimension(100,50));
-            it.setPreferredSize(new Dimension(100,50));
-            atck.setLocation(0,0);
+            run.setPreferredSize(new Dimension(100,50));
             attackBar.add(atck);
-            attackBar.add(it);
+            attackBar.add(run);
             attackBar.add(def);
             attackBar.add(ab);
+            attackBar.add(abBar);
+
             add(attackBar, BorderLayout.SOUTH);
-            attackBar.add(random);
+            pack();
       }
 }
