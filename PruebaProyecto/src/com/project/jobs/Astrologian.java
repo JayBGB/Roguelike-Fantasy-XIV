265545f88738dfc71f7d5528c765abd4d45904ce
Borja G.B <bgarciabarrera@danielcastelao.org>
2023-03-20 09:25:43 +0100

    Finished AST skills, edited README

--- PruebaProyecto/src/com/project/jobs/Astrologian.java
+++ PruebaProyecto/src/com/project/jobs/Astrologian.java
@@ -1,4 +1,34 @@
 package com.project.jobs;
 
+import com.project.game.Main;
+
 public class Astrologian {
+// PLEASE DO THE GOD DAMNED MANA CONSUMPTION THING
+    public void astDamage(){
+
+        Main.data.setDamage(10f+Main.data.getAttIntelligence()*0.4f);
+    }
+
+    public void astMicrocosmos(){
+
+        Main.data.setHp(Main.data.getHp()+(Main.data.getAttCharisma()/2)*3)
+    }
+
+    public void astMacrocosmos(){
+
+        float mcDamage = 10f + Main.data.getAttCharisma()*0.8f;
+        Main.enemy.seteHp(Main.enemy.geteHp()-mcDamage);
+    }
+
+    public void astDivination(){
+
+        Main.data.setDamage((float) (Main.data.getDamage() + (Main.data.getDamage() * 0.2)))
+    }
+
+    public void astExaltation(){
+
+        Main.data.setDefensePercentage(Main.data.getDefensePercentage()*2);
+    }
+
+
 }
--- README.md
+++ README.md
@@ -22,7 +22,27 @@ Para hacer el programa que nos saque de la mugre cuando lo vendamos a la NASA.
 ## HP & Mana
 - HP -> Done.
 - MANA -> Done.
+- MANA COST -> WIP
 ## Inventory
 ## Stat Block selection
 - Attribute selection -> Done.
-## Companions????????????
+## Jobs skills
+- AST - Only mana left.
+- BRD - WIP
+- BLM - WIP
+- DNC - WIP
+- DRK - WIP
+- DRG - WIP
+- GNB - WIP
+- MCH - WIP
+- MNK - WIP
+- NIN - WIP
+- PLD - WIP
+- RPR - WIP
+- RDM - WIP
+- SGE - WIP
+- SAM - WIP
+- SCH - WIP
+- SMN - WIP
+- WAR - Only cooldowns left.
+- WHM
